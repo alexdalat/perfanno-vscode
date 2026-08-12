e6fc565b9c193fff8d5adc74e34e4efa777aee85
Arthur de Araújo Farias <arthur@afarias.org>
2026-08-12 06:03:05 +0200

    Adding perf.out.new and build scripts

---
+++ src/test/example_cpp_project/build.sh
@@ -0,0 +1,5 @@
+g++ -O2 -g -fno-omit-frame-pointer -fno-optimize-sibling-calls -Wall -Wextra main.cpp
+
+perf record --call-graph dwarf ./a.out
+
+erf report --call-graph folded,0,caller,srcline,branch,count --no-children --full-source-path --stdio --stdio-color never --input perf.data > perf.out
